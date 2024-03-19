d578ef16401fd54f91046c3cf5938846a8f4a3df
Shiva Dhakal <theskdhakal@gmail.com>
2024-03-19 11:40:25 +1100

    final

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -109,49 +109,50 @@ export const Home: React.FC = () => {
         <hr className="my-4 border-black" />
         <div className="contents">
           <h1 className="text-center font-bold">Read All Blogs</h1>
-          {!contents && (
+          {contents.length === 0 ? (
             <div style={{ height: "70vh" }}>
               <h1 className="pt-5 text-red-600 text-center">
                 No blogs found!!
               </h1>
             </div>
-          )}
-          {contents?.map((content) => (
-            <div
-              key={content._id}
-              className="border shadow-lg rounded p-4 my-4 relative"
-            >
-              <p className="text-sm text-gray-500">
-                {format(content.createdAt, "MM/dd/yyyy")}
-              </p>
-              <p className="text-gray-600">By: {content.author}</p>
-              <h2 className="text-lg font-bold text-center underline my-2 text-blue-500">
-                {content.title}
-              </h2>
-              <p>
-                {expandedContentIds.includes(content._id)
-                  ? content.post
-                  : `${content.post.split("").slice(0, 300).join("")}...`}
-                <button
-                  className="text-blue-500 underline ml-1"
-                  onClick={() => toggleExpand(content._id)}
-                >
+          ) : (
+            contents?.map((content) => (
+              <div
+                key={content._id}
+                className="border shadow-lg rounded p-4 my-4 relative"
+              >
+                <p className="text-sm text-gray-500">
+                  {format(content.createdAt, "MM/dd/yyyy")}
+                </p>
+                <p className="text-gray-600">By: {content.author}</p>
+                <h2 className="text-lg font-bold text-center underline my-2 text-blue-500">
+                  {content.title}
+                </h2>
+                <p>
                   {expandedContentIds.includes(content._id)
-                    ? "See less"
-                    : "See more"}
-                </button>
-              </p>
+                    ? content.post
+                    : `${content.post.split("").slice(0, 300).join("")}...`}
+                  <button
+                    className="text-blue-500 underline ml-1"
+                    onClick={() => toggleExpand(content._id)}
+                  >
+                    {expandedContentIds.includes(content._id)
+                      ? "See less"
+                      : "See more"}
+                  </button>
+                </p>
 
-              <div className="absolute top-0 right-1 ">
-                <FaDeleteLeft
-                  className="text-red-800 cursor-pointer"
-                  onClick={() => {
-                    handleOnDelete(content._id);
-                  }}
-                />
+                <div className="absolute top-0 right-1 ">
+                  <FaDeleteLeft
+                    className="text-red-800 cursor-pointer"
+                    onClick={() => {
+                      handleOnDelete(content._id);
+                    }}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </MainLayout>
