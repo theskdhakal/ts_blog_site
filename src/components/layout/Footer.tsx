0534cea60c817a012c7f9f7f51c68734a714a7eb
Shiva Dhakal <theskdhakal@gmail.com>
2024-03-22 11:06:25 +1100

    footer updated

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { useAppDispatch } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import { setUser } from "../../pages/login-register/userSlice";
 import { persistor } from "../../store";
 
 const Footer = () => {
   const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.userInfo);
 
   const handleOnLogout = () => {
     //remove from persist
@@ -18,10 +18,13 @@ const Footer = () => {
 
   return (
     <div className="text-center py-3 bg-sky-100  w-full">
-      All rights reserved !
-      <button onClick={handleOnLogout} className="ml-2 text-blue-600">
-        Logout
-      </button>
+      <h4>All rights reserved !</h4>
+
+      {user && (
+        <button onClick={handleOnLogout} className="ml-2 text-blue-600">
+          Logout
+        </button>
+      )}
     </div>
   );
 };
