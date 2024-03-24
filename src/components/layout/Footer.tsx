ce1cdad6ef57eb30212f601203431d65dfdc5e77
Shiva Dhakal <theskdhakal@gmail.com>
2024-03-25 10:23:52 +1100

    userId used to conditionally render the logout button

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,12 @@
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { setUser } from "../../pages/login-register/userSlice";
 import { persistor } from "../../store";
+import { pulledUser } from "./Header";
 
 const Footer = () => {
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.userInfo);
+  const { _id } = user as pulledUser;
 
   const handleOnLogout = () => {
     //remove from persist
@@ -20,7 +22,7 @@ const Footer = () => {
     <div className="text-center py-3 bg-sky-100  w-full">
       <h4>All rights reserved !</h4>
 
-      {user && (
+      {_id && (
         <button onClick={handleOnLogout} className="ml-2 text-blue-600">
           Logout
         </button>
